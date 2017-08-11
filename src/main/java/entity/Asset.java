c832f440f6c8eaf778d15252d247ae8e8f1033b3
arch1tect <843874092@qq.com>
2017-08-11 16:14:11 +0800

    多人资产加加加v2

--- src/main/java/entity/Asset.java
+++ src/main/java/entity/Asset.java
@@ -17,7 +17,11 @@ import javax.persistence.*;
 
 public class Asset {
 
+    @JsonIgnore
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
     @Column(name = "name", length = 20)
     private String name;
 
@@ -32,7 +36,9 @@ public class Asset {
         this.money=money;
     }
 
-
+    public Integer getId() {
+        return id;
+    }
 
     public void setName(String name) {
         this.name = name;
