40933c0ef4cbf82af6062cc60c54b04935474c83
amaoamao <scumchy@gmail.com>
2017-09-10 17:30:46 +0800

    ver 0.1

--- src/main/java/service/impl/JoinServiceImpl.java
+++ src/main/java/service/impl/JoinServiceImpl.java
@@ -2,15 +2,11 @@ package service.impl;
 
 import dao.ApplicationFormDAO;
 import entity.ApplicationForm;
-import job.AccessTokenJob;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import service.JoinService;
-import weixin.popular.api.MessageAPI;
-import weixin.popular.bean.message.massmessage.MassTextMessage;
 
 import javax.transaction.Transactional;
-import java.util.Collections;
 
 /**
  * Copyright (c) 2017 Peter Mao. All rights reserved.
@@ -29,9 +25,6 @@ public class JoinServiceImpl implements JoinService {
     @Override
     public void submit(ApplicationForm applicationForm) {
         applicationFormDAO.addForm(applicationForm);
-        MassTextMessage massMessage = new MassTextMessage("报名成功！具体面试安排我们会在48小时内通过公众号发送到您的微信，请注意查收。");
-        massMessage.setTouser(Collections.singleton(applicationForm.getOpenid()));
-        MessageAPI.messageMassSend(AccessTokenJob.access_token, massMessage);
     }
 
     @Override
