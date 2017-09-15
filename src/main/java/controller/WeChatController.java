98dff3a53ac7cded87dd1229df08b2024d61fb2d
amaoamao <scumchy@gmail.com>
2017-09-15 22:05:52 +0800

    time limit

--- src/main/java/controller/WeChatController.java
+++ src/main/java/controller/WeChatController.java
@@ -48,19 +48,23 @@ public class WeChatController {
                     return new XMLTextMessage(eventMessage.getFromUserName(), eventMessage.getToUserName(), "非常感谢您关注isdc公众订阅号“进退之间”。。社团官网：https://www.scuisdc.org ，社团微博：http://weibo.com/scuisdc。同时您可以直接输入任何意见、建议或者问题，我们将会在一天之内给您回复！再次感谢您的关注！回复【报名】开始填写报名表！报名成功后回复【面试】获取后续面试安排").toXML();
                 }
                 if (eventMessage.getContent().contains("报名")) {
-                    String openid = eventMessage.getFromUserName();
-                    wechatService.saveOpenid(openid);
-                    XMLNewsMessage.Article t = new XMLNewsMessage.Article();
-                    t.setDescription("欢迎你的加入！");
-                    t.setPicurl("http://7xq5uu.com1.z0.glb.clouddn.com/images_want.jpg");
-                    t.setTitle("ISDC招新报名表");
-                    t.setUrl("https://www.scuisdc.org/api/join/" + eventMessage.getFromUserName());
-                    XMLNewsMessage xmlNewsMessage = new XMLNewsMessage(
-                            eventMessage.getFromUserName(),
-                            eventMessage.getToUserName(),
-                            Collections.singletonList(
-                                    t));
-                    return xmlNewsMessage.toXML();
+                    if (System.currentTimeMillis() <= 1505491200000L) {
+                        String openid = eventMessage.getFromUserName();
+                        wechatService.saveOpenid(openid);
+                        XMLNewsMessage.Article t = new XMLNewsMessage.Article();
+                        t.setDescription("欢迎你的加入！");
+                        t.setPicurl("http://7xq5uu.com1.z0.glb.clouddn.com/images_want.jpg");
+                        t.setTitle("ISDC招新报名表");
+                        t.setUrl("https://www.scuisdc.org/api/join/" + eventMessage.getFromUserName());
+                        XMLNewsMessage xmlNewsMessage = new XMLNewsMessage(
+                                eventMessage.getFromUserName(),
+                                eventMessage.getToUserName(),
+                                Collections.singletonList(
+                                        t));
+                        return xmlNewsMessage.toXML();
+                    } else {
+                        return new XMLTextMessage(eventMessage.getFromUserName(), eventMessage.getToUserName(), "报名时间已经截止了哟，非常抱歉！").toXML();
+                    }
                 } else if (eventMessage.getContent().contains("面试")) {
                     Date date = joinService.generateInterviewDate(eventMessage.getFromUserName(), false);
                     return new XMLTextMessage(eventMessage.getFromUserName(), eventMessage.getToUserName(), date == null ? "查询不到您的面试安排，请稍后再试！" : String.format("您的面试时间是：%s，面试地点为二基楼B座405教室，请准时到场！时间安排如有冲突请私戳管理员", new SimpleDateFormat("yyyy-MM-dd HH:mm").format(date))).toXML();
